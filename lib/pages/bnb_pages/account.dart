0db773f20695228bbf776f06d837f905afc98a30
MHTpro <mht.pro1381@gmail.com>
2023-08-08 20:00:42 -0700

    continue

---
+++ lib/pages/bnb_pages/account.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TheAccount extends StatefulWidget {
+  const TheAccount({super.key});
+
+  @override
+  State<TheAccount> createState() => _TheAccountState();
+}
+
+class _TheAccountState extends State<TheAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_project/pages/bnb_pages/account.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +9,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Widget> myPages = const [
+    TheAccount(),
+    TheAccount(),
+  ];
+  int page = 0;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: page,
+        onTap: (value) {
+          setState(
+            () {
+              page = value;
+            },
+          );
+        },
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            label: "My Account",
+            icon: Icon(
+              Icons.person,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "History",
+            icon: Icon(
+              Icons.history,
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: myPages.elementAt(page),
+    );
   }
 }
